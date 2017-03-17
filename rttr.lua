68bd3ac2951a833ece299bd353166415ec6aca67
Koen Visscher <k.visscher@zefiros.eu>
2017-03-17 10:36:49 +0100

    version.h copy

---
+++ rttr.lua
@@ -0,0 +1,4 @@
+
+if os.isfile( zpm.build._currentExportPath .. "/src/rttr/detail/base/version.h" ) == false then
+    zpm.assert( os.copyfile( zpm.build._currentDependency.buildPath .. "/config/version.h", zpm.build._currentExportPath .. "/src/rttr/detail/base/version.h") )
+end
