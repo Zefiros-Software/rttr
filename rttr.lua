5d7732f549e5fdb3113ce2c8aef8ffbd51ce2b52
Koen Visscher <k.visscher@zefiros.eu>
2017-03-17 10:51:03 +0100

    test

--- rttr.lua
+++ rttr.lua
@@ -1,5 +1,6 @@
 
+print("RTTR postextract")
 if os.isfile( zpm.build._currentExportPath .. "/src/rttr/detail/base/version.h" ) == false then
-    print("RTTR postextract")
+    
     zpm.assert( os.copyfile( zpm.build._currentDependency.buildPath .. "/config/version.h", zpm.build._currentExportPath .. "/src/rttr/detail/base/version.h") )
 end
