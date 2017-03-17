53dc95a16e58608bd506a334e49c943ff3ef4908
Koen Visscher <k.visscher@zefiros.eu>
2017-03-17 10:43:53 +0100

    added debug statement in the copy

--- rttr.lua
+++ rttr.lua
@@ -1,4 +1,5 @@
 
 if os.isfile( zpm.build._currentExportPath .. "/src/rttr/detail/base/version.h" ) == false then
+    print("RTTR postextract")
     zpm.assert( os.copyfile( zpm.build._currentDependency.buildPath .. "/config/version.h", zpm.build._currentExportPath .. "/src/rttr/detail/base/version.h") )
 end
